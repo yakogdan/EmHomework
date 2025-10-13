2df051dd2ed23fb4e752c8a071e133b376f05a98
Bogdan <yakogdan@gmail.com>
2025-10-14 01:26:01 +0800

    Добавлена активити для выполнения первого задания по БД.

---
+++ app/src/main/java/com/yakogdan/emhomework/db_network_pattern/task_flower_shop/FlowerActivity.kt
@@ -0,0 +1,26 @@
+package com.yakogdan.emhomework.db_network_pattern.task_flower_shop
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.yakogdan.emhomework.R
+
+class FlowerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_flower)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom,
+            )
+            insets
+        }
+    }
+}
