c7173ff55a9f3795bdb2794f6b5a83a9a814ea88
Bogdan <yakogdan@gmail.com>
2025-10-18 04:30:06 +0800

    Добавлена активити для выполнения задачи с интерцептором

---
+++ app/src/main/java/com/yakogdan/emhomework/db_network_pattern/task_log_interceptor/InterceptorActivity.kt
@@ -0,0 +1,21 @@
+package com.yakogdan.emhomework.db_network_pattern.task_log_interceptor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.yakogdan.emhomework.R
+
+class InterceptorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_interceptor)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
