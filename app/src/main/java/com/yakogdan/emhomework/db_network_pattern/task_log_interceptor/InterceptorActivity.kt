f7a46823a81e61eed26240f063f298114879287c
Bogdan <yakogdan@gmail.com>
2025-10-19 20:13:46 +0800

    Добавлен KinoApiService

--- app/src/main/java/com/yakogdan/emhomework/db_network_pattern/task_log_interceptor/InterceptorActivity.kt
+++ app/src/main/java/com/yakogdan/emhomework/db_network_pattern/task_log_interceptor/InterceptorActivity.kt
@@ -1,13 +1,32 @@
 package com.yakogdan.emhomework.db_network_pattern.task_log_interceptor
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.yakogdan.emhomework.R
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class InterceptorActivity : AppCompatActivity() {
+
+    private val kinoApiService = KinoApiFactory.apiService
+
+    private val btnGetData by lazy {
+        findViewById<Button>(R.id.btnGetData)
+    }
+
+    private val tvInterceptor by lazy {
+        findViewById<TextView>(R.id.tvInterceptor)
+    }
+
+    val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +36,27 @@ class InterceptorActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btnGetData.setOnClickListener {
+
+            val data = kinoApiService.getFilmById(id = 258687)
+            val disposable = data
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        tvInterceptor.text = it.nameOriginal ?: "Empty"
+                    },
+                    {
+                        Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+                    }
+                )
+            compositeDisposable.add(disposable)
+        }
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.dispose()
+        super.onDestroy()
     }
 }
